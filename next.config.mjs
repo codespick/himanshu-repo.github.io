0d42544b3fcb0b58dc853e84b6c45270d82c5b4e
himanshu-profile <gzone2434@gmail.com>
2026-05-22 14:48:45 +0530

    fix github pages root deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? '/himanshu-repo' : '',
-  assetPrefix: isProd ? '/himanshu-repo/' : '',
   images: {
     unoptimized: true,
   },
